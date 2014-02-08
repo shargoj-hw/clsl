d276a3f85faba07cf71f655f908ae5e4c5bd466a
James Shargo <mnudev@gmail.com>
2014-02-07 19:57:06 -0500

    Initial commit!

---
+++ project.clj
@@ -0,0 +1,13 @@
+(defproject clsl "0.1.0-SNAPSHOT"
+  :description "GLSL shaders in clojure made awesome"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/clojure "1.5.1"]
+                 [com.badlogicgames.gdx/gdx "0.9.9"]
+                 [com.badlogicgames.gdx/gdx-backend-lwjgl "0.9.9"]
+                 [com.badlogicgames.gdx/gdx-platform "0.9.9" 
+                  :classifier "natives-desktop"]]
+  :repositories [["sonatype" 
+                  "https://oss.sonatype.org/content/repositories/snapshots/"]])
